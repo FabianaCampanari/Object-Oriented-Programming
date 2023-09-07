0fff83fa2c933b07d73733ebbd1d52f1e551e9f4
Fabiana Campanari <113218619+FabianaCampanari@users.noreply.github.com>
2023-09-07 03:17:41 -0300

    class03_PL

---
+++ class03_PL/dicionarios.py
@@ -0,0 +1,13 @@
+notas = {'Jack': 6.0, 'Anna': 9.0, 'Megan': 8.0}
+
+pessoa = {
+<<<<<<< Updated upstream
+    'nome': 'Jack',
+    'ídade': '40',
+=======
+    'nome': 'Fabiana',
+    'idade': 40,
+>>>>>>> Stashed changes
+    'telefone': '(11) 9999-9999',
+    'notas': notas,
+}
