e0638369ecee67b2b9459ee79365eae61eb7c739
Fabiana Campanari <fabicampanari@gmail.com>
2024-02-25 06:35:31 -0300

    Update aula01a_Lista de prefixos para bases dos números inteiros.py

--- class01_PL/aula01a_Lista de prefixos para bases dos números inteiros.py
+++ class01_PL/aula01a_Lista de prefixos para bases dos números inteiros.py
@@ -1,22 +1,2 @@
 Python 3.11.3 (v3.11.3:f3909b8bc8, Apr  4 2023, 20:12:10) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
->>> #Lista de prefixos para bases dos números inteiros
->>> '''Binaria Prefixo 0b_XXXX_XXXX'''
-'Binaria Prefixo 0b_XXXX_XXXX'
->>> 0b_1110_0101
-229
->>> 
->>> '''Octal(8) Prefixo 0o_XXX'''
-'Octal(8) Prefixo 0o_XXX'
->>> 0o_345
-229
->>> 
->>> '''Decimal Prefixo - NAO POSSUI'''
-'Decimal Prefixo - NAO POSSUI'
->>> 229
-229
->>> 
->>> '''Hexadecimal(16)Prefixo 0x_eX'''
-'Hexadecimal(16)Prefixo 0x_eX'
->>> 0x_e5
-229
